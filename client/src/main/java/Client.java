9e724d3a89cf19f1f3fd3ad06d82b60d3499cd9d
CppCommons <cppcommons@gmail.com>
2017-09-22 02:07:53 +0900

    .

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,4 +1,5 @@
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.swt.internal.Library;
 
 @Slf4j
 public class Client {
@@ -8,6 +9,7 @@ public class Client {
   public static void main(String[] args) {
 	  System.out.println("main");
 	  log.info("main8");
+          log.info(Libs01.add2(22,33));
   }
 
 }
