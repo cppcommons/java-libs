4b15a4a74a71a41c4bd3a6d1dc275b1eea36e954
CppCommons <cppcommons@gmail.com>
2017-09-22 01:56:24 +0900

    .

---
+++ libs.01/src/main/java/Libs01.java
@@ -0,0 +1,11 @@
+
+public class Libs01 {
+
+    public static int add2(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add2(11, 22));
+    }
+}
