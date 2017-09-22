547fa2dc790751ea9ae304830ecc14c5730a5c16
CppCommons <cppcommons@gmail.com>
2017-09-22 09:49:07 +0900

    .

---
+++ java-libs02/src/main/java/Libs02.java
@@ -0,0 +1,11 @@
+
+public class Libs02 {
+
+    public static int sub2(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sub2(11, 22));
+    }
+}
